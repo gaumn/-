c0e34a28d4b7991d92e7528f16ecbd4c96d38988
gaumn <gaumn@outlook.com>
2021-04-08 18:48:08 +0800

    main

---
+++ data_structure/cpp/Queue/Random.hpp
@@ -0,0 +1,39 @@
+/*
+ * @Description: Random.hpp
+ * @Author: gaumn 
+ * @Date: 2021-03-20 17:54:27
+ * @LastEditTime: 2021-03-20 21:45:53
+ * @LastEditors: gaumn
+ */
+#pragma onece
+
+#include <iostream>
+#include <cassert>
+#include <ctime>
+#include <vector>
+using namespace std;
+namespace Random
+{
+    vector<int> get_array(int n, int rangeL, int rangeR)
+    {
+        vector<int> arr;
+        srand(int(time(0)));
+        assert(rangeL <= rangeR);
+        srand(time(NULL));
+        for (int i = 0; i < n; i++)
+        {
+            arr.push_back(rand() % (rangeR - rangeL + 1) + rangeL);
+        }
+        return arr;
+    }
+    //函数模板，打印数组中元素
+    void printarr(vector<int> &arr)
+    {
+        int n = arr.size();
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+} // namespace Random
