be374dce09239838d75f5b2195c623def8fc2f3e
gaumn <gaumn@outlook.com>
2021-04-08 18:48:56 +0800

    main

---
+++ data_structure/cpp/Queue/Queue.cpp
@@ -0,0 +1,7 @@
+/*
+ * @Description: 
+ * @Author: gaumn 
+ * @Date: 2021-04-08 18:48:39
+ * @LastEditTime: 2021-04-08 18:48:39
+ * @LastEditors: gaumn
+ */
---
+++ data_structure/cpp/Queue/Queue.hpp
@@ -0,0 +1,7 @@
+/*
+ * @Description: 
+ * @Author: gaumn 
+ * @Date: 2021-04-08 18:48:30
+ * @LastEditTime: 2021-04-08 18:48:31
+ * @LastEditors: gaumn
+ */
